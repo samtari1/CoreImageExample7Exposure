72c7812d1acee326ea5242904409b4b0f2a9e653
samtari1 <samtariyang@gmail.com>
2026-03-23 22:57:38 -0400

    Initial Commit

---
+++ CoreImageExample7Exposure/CoreImageExample7ExposureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImageExample7ExposureApp.swift
+//  CoreImageExample7Exposure
+//
+//  Created by Quanpeng Yang on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct CoreImageExample7ExposureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreImageExample7ExposureTests/CoreImageExample7ExposureTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImageExample7ExposureTests.swift
+//  CoreImageExample7ExposureTests
+//
+//  Created by Quanpeng Yang on 3/23/26.
+//
+
+import Testing
+@testable import CoreImageExample7Exposure
+
+struct CoreImageExample7ExposureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
